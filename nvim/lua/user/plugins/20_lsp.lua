9e6a13b081959be3bab0e0a979b8ea2539acd512
Neel Bauman <neel.bauman@example.com>
2025-11-05 00:53:05 +0900

    save

--- nvim/lua/user/plugins/20_lsp.lua
+++ nvim/lua/user/plugins/20_lsp.lua
@@ -6,6 +6,8 @@ return {
     "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
+
+
     end,
   },
 
@@ -26,45 +28,48 @@ return {
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- LSPがアタッチされた時の共通設定 (キーマップなど)
       local on_attach = function(client, bufnr)
+        -- キーマップ設定のヘルパー
+        --
         local bufmap = function(mode, lhs, rhs, desc)
           vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, silent = true, noremap = true, desc = "LSP: " .. desc })
         end
 
+        -- 1. プレフィックスなしのキーマップ
+        -- (これらは <leader> を押さないため which-key のポップアップ対象外です)
         bufmap("n", "K", vim.lsp.buf.hover, "Hover")
         bufmap("n", "gd", vim.lsp.buf.definition, "Go to Definition")
-        bufmap("n", "<leader>rn", vim.lsp.buf.rename, "Rename")
-        bufmap("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
-        bufmap("n", "<leader>e", vim.diagnostic.open_float, "Show Diagnostics")
+        
+        -- ★ ご要望のキーマップ
+        bufmap("n", "gl", vim.diagnostic.open_float, "Show Diagnostics")
+
+
+        -- 2. <leader> 付きのキーマップ
+        -- (which-key で表示させやすくするため <leader>rn -> <leader>r, <leader>ca -> <leader>c に変更推奨)
+        bufmap("n", "<leader>r", vim.lsp.buf.rename, "Rename")
+        bufmap("n", "<leader>c", vim.lsp.buf.code_action, "Code Action")
+        -- (もし <leader>rn, <leader>ca のままが良い場合は、下の register も変更が必要です)
+
+
+        -- 3. which-key に <leader> キーマップを登録
+        -- (which-key プラグインがロードされているか安全に確認)
+        local wk_status, wk = pcall(require, "which-key")
+        if wk_status then
+          wk.register({
+            ["<leader>"] = {
+              r = "LSP: Rename",
+              c = "LSP: Code Action",
+              -- (もし <leader>rn, <leader>ca のままの場合)
+              -- r = { n = "LSP: Rename" },
+              -- c = { a = "LSP: Code Action" },
+            }
+          }, {
+            mode = "n",
+            prefix = "<leader>", -- <leader> (スペース) をプレフィックスとして
+            buffer = bufnr,     -- このバッファでのみ有効にする
+          })
+        end
       end
-
-      -- mason-lspconfig の新しいセットアップ方法
-      require("mason-lspconfig").setup({
-        -- 1. 自動インストールしたいLSPサーバー
-        ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "ts_ls",
-          "bashls",
-          "dockerls",
-          "jsonls",
-    	  "yamlls",
-    	  "astro",
-    	  "marksman",
-        },
-
-        -- 2. 各サーバーに渡す設定 (古い setup_handlers の代わり)
-        handlers = {
-          function(server_name)
-            -- on_attach と capabilities を渡す
-            lspconfig[server_name].setup({
-              on_attach = on_attach,
-              capabilities = capabilities,
-            })
-          end,
-        },
-      })
-    end,
+    end
   },
 }
